d7fa4a4f4596aea466862ebb90751998e1fc456e
MANISH <84389544+STEVEARYAN@users.noreply.github.com>
2021-06-30 17:55:34 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,77 +1,43 @@
 #include <stdio.h>
-#include <cs50.h>
-#include <ctype.h>
+#include <assert.h>
 #include <string.h>
+#include <cs50.h>
+#include<ctype.h>
+string ari(string);
 
-string predictin_grade_level(float score);
+string ari(string s)
 
+{ 
+    int l=strlen(s);
+    float word=0;
+    float sentence=0;
+    float letter=0;
+    float res;
 
-int main(void)
-{
- string s,c;
- float count_words=0,count_char=0,count_sentence=0;
- float score;
- s=get_string("enter the paragraph");
- for (int i=0;i<=strlen(s);i++)
-  {
-   if(isalnum(s[i]))
-        {
-        count_char=count_char+1;
-        }
-   else if(s[i]==' ')
-      {
-      count_words=count_words+1;
-      }
-   else if (s[i]=='.' || s[i]=='!'  || s[i]=='?')
-     {
-       count_sentence=count_sentence+1;
-     }
+  for (int i=0; i<=l;i++)
+  {  
+    if(isalnum(s[i]))
+     {letter++;} 
+   
+     if(*(s+i)==' ')   
+       { word++; }
+       
+     if( *(s+i)=='.' || *(s+i)=='?' ||  *(s+i)=='!') 
+       { sentence++; }     
   }
-    
-   score=4.71*(count_char/count_words)+0.5*(count_words/count_sentence)-21.43;
-    
-    c=predictin_grade_level(score);
-    printf("%s",c);
-    
+  res=(4.71*(letter/word)+(0.5*( word/sentence))-21.43);
+  if(res <=1) {return "Kindergarten";}
+  else if(res<=2 ) { return "First/Second Grade";}
+  else if(res<=3)  {return "Third Grade"; }
+  else if(res<=4)  {return "Fourth Grade";}
+  else if(res<=5)  {return "Fifth Grade";}
+  else if(res<=6)  {return "Sixth Grade";}
+  else if(res<=7)  {return "Seventh Grade"; }
+  else if(res<=8)  {return "Eighth Grade";}
+  else if(res<=9)  {return "Ninth Grade";}
+  else if(res<=10)  {return "Tenth Grade";}
+  else if(res<=11)  {return "Eleventh Grade";}
+  else if(res<=12)  {return "Twelfth grade";}
+  else if(res<=13)  {return "College student";}
+  else  {return "Professor";}
 }
-    string predictin_grade_level(float score)
-  {
-      string grade_level;
-    
-      if (score<=1)
-          grade_level="Kindergarten";
-      else if(score<=2)
-           grade_level="First/Second Grade";
-      else if(score<=3)
-           grade_level="Third Grade";
-      else if(score<=4)
-           grade_level="Fourth Grade";
-      else if(score<=5)
-            grade_level="Fifth Grade";
-      else if(score<=6)
-            grade_level="Sixth Grade";
-      else if(score<=7)
-            grade_level="Seventh Grade";
-      else if(score<=8)
-            grade_level="Eighth Grade";
-      else if(score<=9)
-            grade_level="Ninth Grade";
-      else if(score<=10)
-            grade_level="Tenth Grade";
-    
-      else if(score<=11)
-            grade_level="Eleventh Grade";
-      else if(score<=12)
-            grade_level="Twelfth grade";
-      else if(score<=13)
-            grade_level="College student";
-      else if (score <=14)
-            grade_level="Professor";
-    
-         
-        return grade_level;
-    }
-    
-     
-    
-     
